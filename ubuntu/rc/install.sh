f15ca3ff5373e8f25712cccd06ce5f0c5023ae3d
sx <canivs@gmail.com>
2012-02-27 18:30:38 +0800

    Add some files.

---
+++ ubuntu/rc/install.sh
@@ -0,0 +1,7 @@
+#install default configure for ubuntu
+echo "install default configure for ubuntu"
+
+cp ./bashrc $HOME/.bashrc
+cp ./minirc.dfl $HOME/.minirc.dfl
+cp ./gtktermrc $HOME/.gtktermrc
+cp ./gitconfig $HOME/.gitconfig
